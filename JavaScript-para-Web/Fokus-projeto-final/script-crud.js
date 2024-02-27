0f329bc621b907743c4ab6dfe38078a0ed606ba0
Renan Kasper <renan.kasper18@gmail.com>
2024-02-26 23:17:34 -0300

    Update script-crud.js

--- JavaScript-para-Web/Fokus-projeto-final/script-crud.js
+++ JavaScript-para-Web/Fokus-projeto-final/script-crud.js
@@ -3,13 +3,17 @@
 const btnAdicionarTarefa = document.querySelector('.app__button--add-task')
 const formAdicionarTarefa = document.querySelector('.app__form-add-task')
 const textarea = document.querySelector('.app__form-textarea')
-const ulTarefa = document.querySelector('.app__section-task-list')
+const ulTarefas = document.querySelector('.app__section-task-list')
 
-const tarefas = JSON.parse(localStorage.getItem('tarefas')) || {}
+const tarefas = JSON.parse(localStorage.getItem('tarefas')) || []
+
+function atualizarTarefas () {
+    localStorage.setItem('tarefas', JSON.stringify(tarefas))
+}
 
 function criarElementoTarefa(tarefa) {
     const li = document.createElement('li')
-    li.classList.add('app__section-task-icon-status')
+    li.classList.add('app__section-task-list-item')
 
     const svg = document.createElement('svg')
     svg.innerHTML = `
@@ -18,25 +22,37 @@ function criarElementoTarefa(tarefa) {
             <circle cx="12" cy="12" r="12" fill="#FFF"></circle>
             <path d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z"
                 fill="#01080E"></path>
-        </svg> 
+        </svg>
     `
     const paragrafo = document.createElement('p')
-    paragrafo.innerText = tarefa.descricao
+    paragrafo.textContent = tarefa.descricao
     paragrafo.classList.add('app__section-task-list-item-description')
 
     const botao = document.createElement('button')
     botao.classList.add('app_button-edit')
-    
+
+    botao.onclick = () => {
+        debugger
+        const novaDescricao = prompt("Qual é o nome da tarefa?")
+        console.log('Nova descrição da tarefa: ', novaDescricao)
+        if (novaDescricao) {
+            paragrafo.textContent = novaDescricao
+            tarefa.descricao = novaDescricao
+            atualizarTarefas()
+        }
+    }
+
     const imagemBotao = document.createElement('img')
     imagemBotao.setAttribute('src', '/imagens/edit.png')
-    botao.append('imagemBotao')
+    botao.append(imagemBotao)
 
     li.append(svg)
     li.append(paragrafo)
-    li.append(botao) 
+    li.append(botao)
 
     return li
-}
+}   
+
 btnAdicionarTarefa.addEventListener('click', () => {
     formAdicionarTarefa.classList.toggle('hidden')
 })
@@ -47,14 +63,14 @@ formAdicionarTarefa.addEventListener('submit', (evento) => {
         descricao: textarea.value
     }
     tarefas.push(tarefa)
-    localStorages.setItem('tarefa', JSON.stringify(tarefas))
-    ulTarefa.append(elementoTarefa)
-    localStorage.setItem('tarefa', JSON.stringify(tarefas))
+    const elementoTarefa = criarElementoTarefa(tarefa)
+    ulTarefas.append(elementoTarefa)
+    atualizarTarefas() 
     textarea.value = ''
     formAdicionarTarefa.classList.add('hidden')
 })
 
 tarefas.forEach(tarefa => {
     const elementoTarefa = criarElementoTarefa(tarefa)
-    ulTarefa.append(elementoTarefa)
+    ulTarefas.append(elementoTarefa)
 });
